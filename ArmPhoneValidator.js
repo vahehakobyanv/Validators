c27cecf20b7a8f8a3ece87db6eaa660f11dd31cb
vahehakobyanv <vahehakobyanv@gmail.com>
2017-10-18 12:26:38 +0400

    add some tests

--- ArmPhoneValidator.js
+++ ArmPhoneValidator.js
@@ -1,10 +1,10 @@
 const Validator = require('./Validators');
 class ArmPhoneValidator extends Validator{
   isArmPhoneNumberValid(number){
-    if(!number || !super.isNumber(number))
+    if(!number)
       return false;
     let ValidNumberRegExp = /^(093\d{6}|095\d{6}|077\d{6}|055\d{6}|094\d{6}|098\d{6}|091\d{6}|099\d{6}|041\d{6}|043\d{6}|010\d{6}|[+]374\d{8}|[+]37410\d{6})$/;
-    if(phonenumRegExp.test(number))
+    if(ValidNumberRegExp.test(number))
       return true;
     return false;
   }
